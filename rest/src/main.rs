76f492fe38eb072017b04ec4c898d748cd531920
yellowhatpro <yellowhatpro3119@gmail.com>
2023-03-25 01:04:17 +0530

    error handling rs

--- rest/src/main.rs
+++ rest/src/main.rs
@@ -1,14 +1,41 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main(){
     let args : Vec<String>= env::args().collect();
-    let path = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing args: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = run(config) {
+        println!("Application error: {}",e);
+        process::exit(1);
+    }
+}
 
-    let contents = fs::read_to_string(path)
-        .expect("No such file exists");
-
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.path)?;
     println!("This is the file contents:\n {} ", contents);
+    Ok(())
+}
 
+struct Config {
+    pattern: String,
+    path: String
+}
 
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len()<3 {
+            return Err("Not enough args");
+        }
+        let pattern = args[1].clone();
+        let path = args[2].clone();
+        Ok(Config{
+            pattern,
+            path
+        })
+    }
 }
