ad57a84dabe846d98b1c2ccbfe02f597bb0184b2
yellowhatpro <yellowhatpro3119@gmail.com>
2023-09-19 22:08:32 +0530

    traits

--- lets_get_rusty/src/bin/chapters/chapter10_2.rs
+++ lets_get_rusty/src/bin/chapters/chapter10_2.rs
@@ -1,9 +1,38 @@
 pub mod traits {
-    //Same as
+    //Same as interfaces
+
+    pub struct NewsArticle {
+        pub author: String,
+        pub headline: String,
+        pub content: String,
+    }
+
+    pub struct Tweet {
+        pub username: String,
+        pub content: String,
+        pub reply: String,
+        pub retweet: String,
+    }
+
+    impl Summary for Tweet {
+        fn summarize(&self) -> String {
+            format!("{}: {}", self.username, self.content)
+        }
+    }
 
     pub trait Summary {
-        fn summarize(&self) -> String;
+        fn summarize(&self) -> String {
+            String::from("(Read more...)")
+        }
     }
 
     pub fn traits() {}
+
+    fn main() {
+        let article = NewsArticle {
+            author: String::from("Ashu Aswal"),
+            headline: String::from("The sky is falling"),
+            content: String::from("The sky is not actually falling"),
+        };
+    }
 }
